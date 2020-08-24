20f8aec592f90de6fdfe5f07db8c3d942da04834
atgianne <atgianne@outlook.com>
2020-08-24 07:24:21 +0300

    Update TpmManager.h

--- include/TpmManager.h
+++ include/TpmManager.h
@@ -15,9 +15,9 @@ public:
     void initialize_new_context(TSS_CONTEXT **ctx);
     void start_authorization_session(TSS_CONTEXT *ctx, TPMI_SH_POLICY *sessionHandle, TPM_SE sessionType);
     void flush_context(TSS_CONTEXT *ctx, TPM_HANDLE handle);
-
+    PCR_Read_Out readPCR(TSS_CONTEXT *ctx, int pcr);
     void pcrExtend(TSS_CONTEXT *ctx, unsigned char *binaryHash, uint8_t pcr);
-    CertifyCreation_Out certify_creation(TSS_CONTEXT* ctx, TPM_HANDLE& signing_key, TPM_HANDLE& certifiable_key, TPM2B_DIGEST& creation_hash, TPMT_TK_CREATION& creation_ticket, TPM_HANDLE* sessionHandle, const char* pass);
+    CertifyCreation_Out certify_creation(TSS_CONTEXT* ctx, TPM_HANDLE& signing_key, TPM_HANDLE& certifiable_key, TPM2B_DIGEST& creation_hash, TPMT_TK_CREATION& creation_ticket, const TPM_HANDLE* sessionHandle, const char* pass);
     Quote_Out getQuote(TSS_CONTEXT* ctx, TPML_PCR_SELECTION& pcrs, TPM2B_DATA* extra, TPM_HANDLE signingKey, const char* signingKeyPass);
     TPMT_SIGNATURE sign(TSS_CONTEXT* ctx, TPM2B_DIGEST* digest, TPM_HANDLE key, const TPM_HANDLE* policySession, TPMT_TK_HASHCHECK *validation, int* err);
     CreatePrimary_Out create_primary_key(TSS_CONTEXT* ctx, TPMI_RH_HIERARCHY hierarchy, TPM2B_PUBLIC& pubTemplate);
